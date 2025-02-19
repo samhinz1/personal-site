989caa171b03e77b8c6613bdc7ee55d5474cb18c
Sam Hinz <samhinz@live.com>
2025-02-19 14:45:46 +1000

    and again

--- src/components/CaseStudies.tsx
+++ src/components/CaseStudies.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
-import artwologogreen from '../static/artwologogreen.png';
+import artwologogreen from '../static/ArtwoLogoGreen.png';
 
 const caseStudies = [
   {
